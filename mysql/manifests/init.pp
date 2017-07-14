30c781ff105b62aa07f7ffbe1f31fe580c77638a
dmiralles <davidmifo@gmail.com>
2017-07-14 16:25:03 +0200

    Fix

--- mysql/manifests/init.pp
+++ mysql/manifests/init.pp
@@ -59,7 +59,8 @@ class mysql (
                 command => '/bin/systemctl stop mysql',
                 onlyif  => '/bin/systemctl status mysql',
                 require => Package['mysql-server','python-mysqldb','mysql-common'],
-        }~>
+                notify  => File["/etc/mysql/mysql.conf.d/mysqld.cnf"],
+        }
         file {'/etc/mysql/mysql.conf.d/mysqld.cnf':
                 mode    => "0644",
                 replace => "true",
