5654c8bd7a448bbf9599ad420c3cc728789f4310
dmiralles <davidmifo@gmail.com>
2017-07-14 11:43:43 +0200

    Adding mysql configuration

--- mysql/manifests/init.pp
+++ mysql/manifests/init.pp
@@ -54,5 +54,15 @@ class mysql (
                 command => "mysql -u root -p$mysql_password $ospos_db_name < /tmp/database.sql",
                 require => Exec["Create OSPOS user for database"],
         }
-
+        file {'/etc/mysql/mysql.conf.d/mysqld.cnf':
+                mode    => "0644",
+                replace => "true",
+                source  => 'puppet:///modules/mysql/mysqld.cnf',
+                notify  => Service["mysql"],
+        }
+        service { 'mysql':
+                ensure  => 'running',
+                enable  => true,
+                require => Package["mysql-server"],
+        }
 }
