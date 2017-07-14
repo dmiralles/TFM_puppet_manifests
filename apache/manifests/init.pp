e2f966467d0c7eea5eea5dbc4b1bffd9bf2b8a35
dmiralles <davidmifo@gmail.com>
2017-07-14 21:07:50 +0200

    Commiting

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -10,4 +10,9 @@ class apache {
     ensure => absent,
     require => Package["apache2"],
   }
+  exec {'Enable Rewrite':
+    command => '/usr/sbin/a2enmod rewrite',
+    notify  => Service['apache2'], 
+    require => [Package['apache2'],File['/var/www/html/index.html']],
+  }
 }
