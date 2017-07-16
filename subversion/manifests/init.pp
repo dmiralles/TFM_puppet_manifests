099b07e1a724382ec323f3a47d539536a16e381d
dmiralles <davidmifo@gmail.com>
2017-07-17 00:11:26 +0200

    Adding SVN customizations

--- subversion/manifests/init.pp
+++ subversion/manifests/init.pp
@@ -18,14 +18,34 @@ class subversion (
     recurse => true,
     require => [Package["subversion"],Package["libapache2-svn"],Package["libapache2-mod-svn"],Package["libsvn-dev"]],
   }
+  file {"$svn_dir/conf/svnserve.conf":
+          ensure  => present,
+          replate => true,
+          source  => 'puppet:///modules/subversion/svnserve.conf',
+          require => Exec["$svn_dir"],
+  }
   exec { "Create Repository":
           command => "/usr/bin/svnadmin create $svn_dir",
           cwd     => "/tmp",
           require => File["$svn_dir"],
   }
   exec { "Create Repository":
+          command => "/usr/bin/svnserve -d",
+          require => [File["$svn_dir/conf/svnserve.conf"], Exec["Create Repository"]],
+  }
+  exec { "Enable a2enmod":
           command => "/usr/bin/a2enmod dav_svn",
           cwd     => "/tmp",
-          require => File["Create Repository"],
+          require => Exec["Create Repository"],
+  }
+  file {"/etc/apache2/mods-available/dav_svn.conf":
+          ensure  => present,
+          replate => true,
+          content => template("subversion/dav_svn.conf.erb"),
+          require => Exec["Enable a2enmod"],
+  }
+  exec { "Restart Apache":
+          command => "/bin/systemctl restart apache2",
+          require => File["/etc/apache2/mods-available/dav_svn.conf"],
   }
 }
