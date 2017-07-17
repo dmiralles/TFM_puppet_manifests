f0ce68c70f670690fda63d298489205ed08d7822
dmiralles <davidmifo@gmail.com>
2017-07-17 13:28:25 +0200

    commit packaging version

--- subversion/files/initial_script.sh
+++ subversion/files/initial_script.sh
@@ -9,14 +9,13 @@ svn mkdir file:///opt/subversion/ospos -m "Create new folder"
 svn checkout file:///opt/subversion
 cd /opt/subversion/subversion/ospos
 #Empaquetando OSPOS con version de PR
-PR_NUMBER=1
 cd /opt/subversion/subversion/ospos
 echo "Compressing code"
-tar -zcf opensourcepos-${PR_NUMBER}.tar.gz -C /tmp/ospos .
+tar -zcf opensourcepos-latest.tar.gz -C /tmp/ospos .
 echo "Compressed file completed"
 
 #Envio a SVN el paquete instalando cliente
 echo "Adding package to SVN"
-svn add opensourcepos-${PR_NUMBER}.tar.gz
+svn add opensourcepos-latest.tar.gz
 echo "Commiting to SVN"
 svn commit -m "First commit"
