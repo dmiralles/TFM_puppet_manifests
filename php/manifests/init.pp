842fdb00220d992fac67b3121f6d8d91101f2f8c
dmiralles <davidmifo@gmail.com>
2017-07-17 19:11:16 +0200

    Adding version 5.6 of php

--- php/manifests/init.pp
+++ php/manifests/init.pp
@@ -2,19 +2,19 @@ class php {
         exec {'Update repos':
                 command => '/usr/bin/apt-get update',
         }
-        package {'php5':
+        package {'php5.6':
                 ensure  => present,
                 require => Exec["Update repos"],
         }
-        package {'php5-mysql':
+        package {'php5.6-mysql':
                 ensure => present,
                 require => Exec["Update repos"],
         }
-        package {'libapache2-mod-php5':
+        package {'libapache2-mod-php5.6':
                 ensure => present,
                 require => Exec["Update repos"],
         }
-        package {'php5-mcrypt':
+        package {'php5.6-mcrypt':
                 ensure => present,
                 require => Exec["Update repos"],
         }
