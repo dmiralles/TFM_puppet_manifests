4582baa709fd43a88be80e63da996580a531988a
dmiralles <davidmifo@gmail.com>
2017-07-17 12:48:49 +0200

    Commiting

--- subversion/files/initial_script.sh
+++ subversion/files/initial_script.sh
@@ -3,17 +3,17 @@ echo "Downloading code from GitHub"
 git clone https://github.com/dmiralles/ospos
 cd /tmp/ospos
 echo "Code downloaded"
-#Empaquetando OSPOS con version de PR
-PR_NUMBER=1
-cd /opt/subversion
-echo "Compressing code"
-tar -zcvf opensourcepos-${PR_NUMBER}.tar.gz -C /opt/subversion
-echo "Compressed file completed"
 
 #Dejando listo SVN
 svn mkdir file:///opt/subversion/ospos -m "Create new folder"
 svn checkout file:///opt/subversion
 cd /opt/subversion/subversion/ospos
+#Empaquetando OSPOS con version de PR
+PR_NUMBER=1
+cd /opt/subversion
+echo "Compressing code"
+tar -zcvf opensourcepos-${PR_NUMBER}.tar.gz -C /tmp/ospos .
+echo "Compressed file completed"
 
 #Envio a SVN el paquete instalando cliente
 echo "Adding package to SVN"
