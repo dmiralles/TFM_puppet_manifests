7093d87612acb8f6235dbc1de2d712cf52254e87
dmiralles <davidmifo@gmail.com>
2017-07-17 12:38:18 +0200

    Fixing SVN issues for checking out

--- subversion/files/initial_script.sh
+++ subversion/files/initial_script.sh
@@ -1,19 +1,22 @@
 cd /tmp
+echo "Downloading code from GitHub"
 git clone https://github.com/dmiralles/ospos
 cd /tmp/ospos
-
+echo "Code downloaded"
 #Empaquetando OSPOS con version de PR
 PR_NUMBER=1
 cd /opt/subversion
-tar -zcvf opensourcepos-${PR_NUMBER}.tar.gz -C /opt/subversion .
+echo "Compressing code"
+tar -zcvf opensourcepos-${PR_NUMBER}.tar.gz -C /opt/subversion
+echo "Compressed file completed"
 
 #Dejando listo SVN
 svn mkdir file:///opt/subversion/ospos -m "Create new folder"
-svn checkout /opt/subversion
+svn checkout file:///opt/subversion
 cd /opt/subversion/subversion/ospos
 
 #Envio a SVN el paquete instalando cliente
 echo "Adding package to SVN"
 svn add opensourcepos-${PR_NUMBER}.tar.gz
 echo "Commiting to SVN"
-svn commit -m "First commit
+svn commit -m "First commit"
