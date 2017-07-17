295b88cad86800f9741c2a0a876d608f1cf385fe
dmiralles <davidmifo@gmail.com>
2017-07-17 16:34:42 +0200

    lowering to PHP5

--- php/manifests/init.pp
+++ php/manifests/init.pp
@@ -1,14 +1,14 @@
 class php {
-        package {'php5.6':
+        package {'php5':
                 ensure => present,
         }
-        package {'php5.6-mysql':
+        package {'php5-mysql':
                 ensure => present,
         }
-        package {'libapache2-mod-php5.6':
+        package {'libapache2-mod-php5':
                 ensure => present,
         }
-        package {'php5.6-mcrypt':
+        package {'php5-mcrypt':
                 ensure => present,
         }
 }
