579415782637ef5369a88b747e637a8e455ada6e
dmiralles <davidmifo@gmail.com>
2017-07-17 16:55:51 +0200

    Adding repos to update

--- php/manifests/init.pp
+++ php/manifests/init.pp
@@ -1,14 +1,25 @@
 class php {
+        exec {'Get php5':
+                command => '/usr/bin/add-apt-repository ppa:ondrej/php',
+        }
+        exec {'Update repos':
+                command => '/usr/bin/apt-get update',
+                require => Exec["Get php5"],
+        }
         package {'php5':
-                ensure => present,
+                ensure  => present,
+                require => Exec["Update repos"],
         }
         package {'php5-mysql':
                 ensure => present,
+                require => Exec["Update repos"],
         }
         package {'libapache2-mod-php5':
                 ensure => present,
+                require => Exec["Update repos"],
         }
         package {'php5-mcrypt':
                 ensure => present,
+                require => Exec["Update repos"],
         }
 }
