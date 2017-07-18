4a9561ac4c68959366f0d3ad56925f453872c784
dmiralles <davidmifo@gmail.com>
2017-07-19 00:39:18 +0200

    Fixing

--- mysql/manifests/init.pp
+++ mysql/manifests/init.pp
@@ -1,6 +1,6 @@
 class mysql {
-        Class["mysql::install"]~>
-        Class["mysql::config"]~>
-        Class["mysql::custom"]~>
-        Class["mysql::service"]
+        Class["install"]~>
+        Class["config"]~>
+        Class["custom"]~>
+        Class["service"]
 }
