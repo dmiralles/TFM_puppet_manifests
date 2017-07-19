1776cb336bf1bb123586a068ddfdde8b37f2a994
dmiralles <davidmifo@gmail.com>
2017-07-19 19:23:58 +0200

    Fixing syntax errors

--- ospos/manifests/maintenance.pp
+++ ospos/manifests/maintenance.pp
@@ -1,10 +1,10 @@
 class ospos::maintenance(
   $version = 'latest',
 ){
-  Exec {'Remove working path':
+  exec {'Remove working path':
         command => '/bin/rm -rf /tmp/ospos',
   }
-  file {'Remove previous package':
-        command => '/bin/rm -rf /tmp/opensourcepos-$version.tar.gz'.       
+  exec {'Remove previous package':
+        command => '/bin/rm -rf /tmp/opensourcepos-$version.tar.gz',      
   }
 }
